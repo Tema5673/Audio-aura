c1fdc7b932ad04939ff2fb0bfdce0c70a5d368e7
Tema <public@mail.ru>
2024-05-17 11:53:47 +0300

    add

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# Python код
-
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -12,28 +10,19 @@ app.config['SECRET_KEY'] = 'Pantera55285'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)  # Initialize Flask-Migrate
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
-    tags = db.Column(db.String(150), nullable=True)  # Ensure this line is present
-
-
-def create_tables():
-    with app.app_context():
-        db.create_all()
-
+    tags = db.Column(db.String(150), nullable=True)
 
 @app.route('/')
 def index():
     return render_template('main/search.html')
 
-
 @app.route('/personal_area', methods=['GET', 'POST'])
 def personal_area():
     return render_template('main/personal_area.html')
-
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -41,7 +30,6 @@ def register():
     password = request.form['password']
     confirm_password = request.form['confirm-password']
 
-    # Проверка наличия пользователя с таким именем
     existing_user = User.query.filter_by(username=username).first()
     if existing_user:
         flash('Пользователь с таким логином уже существует!', 'error')
@@ -55,10 +43,7 @@ def register():
     db.session.add(new_user)
     db.session.commit()
     flash('Регистрация прошла успешно!', 'success')
-
     return jsonify({'status': 'success'}), 200
-
-
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -73,14 +58,9 @@ def login():
         flash('Неверный логин или пароль!', 'error')
         return redirect(url_for('personal_area'))
 
-
 @app.route('/logout')
 def logout():
-    # Логика выхода из аккаунта (например, удаление сессионных данных)
-
     return redirect(url_for('index'))
 
-
 if __name__ == '__main__':
-    create_tables()
     app.run(debug=True)
